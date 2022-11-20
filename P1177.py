89b7333be494c23841da8043c88ab18f0b2364ca
xiluoteng <118146774+xiluoteng@users.noreply.github.com>
2022-11-20 21:22:21 +0900

    Update P1177.py

--- P1177.py
+++ P1177.py
@@ -1,9 +1,9 @@
 N=int(input())
 a=input().split(' ')
-list=[]
+list1=[]
 for i in range(N):
     a[i]=int(a[i])
-    list.append(a[i])
-list.sort(reverse=False)
-for i in list:
+    list1.append(a[i])
+list1.sort(reverse=False)
+for i in list1:
     print(i,end=" ")
